31a01ae7b4c8d826439fce94453011f9ee67e388
Cyril-dot <chukwukaottih@gmail.com>
2026-03-24 17:01:29 +0000

    update ping

--- src/main/java/com/bettingPlatform/BettingWebsite/Config/Security/Scheduler.java
+++ src/main/java/com/bettingPlatform/BettingWebsite/Config/Security/Scheduler.java
@@ -23,9 +23,8 @@ public class Scheduler {
     }
 
     // Ping every 5 minutes (300,000 ms)
-    @Scheduled(fixedRate = 300000)
+    @Scheduled(fixedRate = 30000) // 30 seconds
     public void keepAlive() {
-        pingRender();
         pingDatabase();
     }
 
